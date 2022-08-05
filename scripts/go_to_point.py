78ca02550e907319d1d4d8ad060d9013c0f50b1f
Riccardo Zuppetti <89387809+RiccardoZuppetti@users.noreply.github.com>
2022-08-05 10:48:31 +0200

    Add files via upload

--- scripts/go_to_point.py
+++ scripts/go_to_point.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
-
 import rospy
 from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
 from tf import transformations
-from rt2_assignment1.srv import Position
 import math
+import actionlib
+import rt2_assignment1.msg
 
 # robot state variables
 position_ = Point()
@@ -14,6 +14,11 @@ yaw_ = 0
 position_ = 0
 state_ = 0
 pub_ = None
+
+# Auxiliary variables
+des_position = Point()
+des_position.z = 0
+temp_flag = False
 
 # parameters for control
 yaw_precision_ = math.pi / 9  # +/- 20 degree allowed
@@ -24,6 +29,9 @@ kp_d = 0.2
 ub_a = 0.6
 lb_a = -0.5
 ub_d = 0.6
+
+# parameter for action
+action_server = None
 
 def clbk_odom(msg):
     global position_
@@ -41,12 +49,10 @@ def clbk_odom(msg):
     euler = transformations.euler_from_quaternion(quaternion)
     yaw_ = euler[2]
 
-
 def change_state(state):
     global state_
     state_ = state
     print ('State changed to [%s]' % state_)
-
 
 def normalize_angle(angle):
     if(math.fabs(angle) > math.pi):
@@ -54,6 +60,7 @@ def normalize_angle(angle):
     return angle
 
 def fix_yaw(des_pos):
+    global yaw_, pub_, yaw_precision_2_, state_
     desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
     err_yaw = normalize_angle(desired_yaw - yaw_)
     rospy.loginfo(err_yaw)
@@ -72,6 +79,7 @@ def fix_yaw(des_pos):
 
 
 def go_straight_ahead(des_pos):
+    global yaw_, pub_, yaw_precision_, state_
     desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
     err_yaw = desired_yaw - yaw_
     err_pos = math.sqrt(pow(des_pos.y - position_.y, 2) +
@@ -88,12 +96,12 @@ def go_straight_ahead(des_pos):
         twist_msg.angular.z = kp_a*err_yaw
         pub_.publish(twist_msg)
     else: # state change conditions
-        #print ('Position error: [%s]' % err_pos)
+        # print ('Position error: [%s]' % err_pos)
         change_state(2)
 
     # state change conditions
     if math.fabs(err_yaw) > yaw_precision_:
-        #print ('Yaw error: [%s]' % err_yaw)
+        # print ('Yaw error: [%s]' % err_yaw)
         change_state(0)
 
 def fix_final_yaw(des_yaw):
@@ -117,31 +125,54 @@ def done():
     twist_msg.linear.x = 0
     twist_msg.angular.z = 0
     pub_.publish(twist_msg)
+    action_server.set_succeeded()
+    temp_flag = True
     
-def go_to_point(req):
-    desired_position = Point()
-    desired_position.x = req.x
-    desired_position.y = req.y
-    des_yaw = req.theta
+def go_to_point(goal):
+    global state_, des_position, action_server, temp_flag
+    des_position.x = goal.target_pose.pose.position.x
+    des_position.y = goal.target_pose.pose.position.y
+    des_yaw = goal.target_pose.pose.position.z
     change_state(0)
     while True:
-    	if state_ == 0:
-    		fix_yaw(desired_position)
-    	elif state_ == 1:
-    		go_straight_ahead(desired_position)
-    	elif state_ == 2:
-    		fix_final_yaw(des_yaw)
-    	elif state_ == 3:
-    		done()
-    		break
+        # if the action is preempted
+        if action_server.is_preempt_requested():
+            rospy.loginfo('Goal was preempted')
+            # publish a null velocity
+            twist_msg = Twist()
+            twist_msg.linear.x = 0
+            twist_msg.angular.z = 0
+            pub_.publish(twist_msg)
+            # goal preempted
+            action_server.set_preempted()
+            temp_flag = False
+            break
+        elif state_ == 0:
+            # orientation towards the goal
+            fix_yaw(des_position)  
+        elif state_ == 1:
+            # moving in straight direction
+            go_straight_ahead(des_position)   
+        elif state_ == 2:
+            # final orientation
+            fix_final_yaw(des_yaw) 
+        elif state_ == 3:
+            # goal reached
+            done()
+            break
     return True
 
 def main():
-    global pub_
+    global pub_, action_server
+    # initialize the goal
     rospy.init_node('go_to_point')
+    # publish the velocity
     pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    # subscriber to the /odom topic
     sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
-    service = rospy.Service('/go_to_point', Position, go_to_point)
+    # initialize the action server
+    action_server = actionlib.SimpleActionServer('/go_to_point', rt2_assignment1.msg.PlanningAction, go_to_point, auto_start=False)
+    action_server.start()
     rospy.spin()
 
 if __name__ == '__main__':
