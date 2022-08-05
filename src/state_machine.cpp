22978f2ab7bdd0eca2760bdc037d6073dad45f89
Riccardo Zuppetti <89387809+RiccardoZuppetti@users.noreply.github.com>
2022-08-05 10:57:57 +0200

    Update state_machine.cpp

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -19,49 +19,48 @@ bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Com
 }
 
 
-int main(int argc, char **argv)
-{
-   ros::init(argc, argv, "state_machine");
-   ros::NodeHandle n;
-   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
-   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
-   actionlib::SimpleActionClient<rt2_assignment1::PlanningAction> ac("/go_to_point");
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "state_machine");
+	ros::NodeHandle n;
+	ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
+	ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
+	actionlib::SimpleActionClient<rt2_assignment1::PlanningAction> ac("/go_to_point");
+	rt2_assignment1::RandomPosition rp;
+	rp.request.x_max = 5.0;
+	rp.request.x_min = -5.0;
+	rp.request.y_max = 5.0;	
+	rp.request.y_min = -5.0;
    
-   rt2_assignment1::RandomPosition rp;
-   rp.request.x_max = 5.0;
-   rp.request.x_min = -5.0;
-   rp.request.y_max = 5.0;
-   rp.request.y_min = -5.0;
-   
-   while(ros::ok()){
-        ros::spinOnce();
-   	    if (start){
-            if(!moving){
-   		        client_rp.call(rp);
-                rt2_assignment1::PlanningGoal goal;
-			    goal.target_pose.header.frame_id = "base_link";
-			    goal.target_pose.header.stamp = ros::Time::now();
-			    goal.target_pose.pose.position.x = rp.response.x;
-		        goal.target_pose.pose.position.y = rp.response.y;
-			    goal.target_pose.pose.orientation.z = rp.response.theta;
-			    std::cout << "\nGoing to the position: x= " << rp.response.x << " y= " <<rp.response.y << " theta = " <<rp.response.theta << std::endl;
-			    ac.sendGoal(goal);
-		        moving = true;
-            }
-            else{
-                if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-				    std::cout << "Goal completed" << std::endl;
+	while(ros::ok()){
+		ros::spinOnce();
+		if (start){
+   			if(!moving){
+   				client_rp.call(rp);
+                		rt2_assignment1::PlanningGoal goal;
+				goal.target_pose.header.frame_id = "base_link";
+				goal.target_pose.header.stamp = ros::Time::now();
+				goal.target_pose.pose.position.x = rp.response.x;
+		       		goal.target_pose.pose.position.y = rp.response.y;
+				goal.target_pose.pose.orientation.z = rp.response.theta;
+				std::cout << "\nGoing to the position: x= " << rp.response.x << " y= " <<rp.response.y << " theta = " <<rp.response.theta << std::endl;
+				ac.sendGoal(goal);
+		      		moving = true;
+            		}
+    			else{
+                		if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
+					std::cout << "Goal completed" << std::endl;
 					moving = false;
 				}
-            }
-   	    }
-        else{
-            if(moving){
-            	ac.cancelAllGoals();
+            		}
+  		}
+        	else{
+     			if(moving){
+            			ac.cancelAllGoals();
 				std::cout << "Goal cancelled" << std::endl;
 				moving = false;
-            }
-        }
-   }
-   return 0;
+            		}
+        	}
+	}
+	return 0;
 }
